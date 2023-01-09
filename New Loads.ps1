5c6b556eb844c081429f6d6b9ef994380442e75f
circlol <65281163+circlol@users.noreply.github.com>
2023-01-08 20:21:58 -0800

    Update New Loads.ps1

--- New Loads.ps1
+++ New Loads.ps1
@@ -15,7 +15,8 @@ Function Programs() {
             Write-Status -Types "+", "Registry" -Status "Flagging Google Chrome to Install UBlock Origin"
             REG ADD $PathToChromeExtensions /v update_url /t REG_SZ /d $PathToChromeLink /f | Out-Null
             Check
-        }else {
+        }
+        else {
             CheckNetworkStatus
             Write-Status -Types "+", $TweakType -Status "Downloading Google Chrome"
             Start-BitsTransfer -Source $package1dl -Destination $package1lc -TransferType Download -RetryInterval 60 -RetryTimeout 60 -Verbose | Out-Host
@@ -25,7 +26,9 @@ Function Programs() {
             Write-Status -Types "+", "Registry" -Status "Flagging Google Chrome to Install UBlock Origin"
             REG ADD $PathToChromeExtensions /v update_url /t REG_SZ /d $PathToChromeLink /f | Out-Null
             Check
-    }}else {
+        }
+    }
+    else {
         Write-Status -Types "?", $TweakType -Status "Google Chrome is already Installed on this PC." -warning
     }
 
@@ -37,14 +40,17 @@ Function Programs() {
         If (Test-Path -Path:$vlcoi) {
             Write-Status -Types "+", $TweakType -Status "Installing VLC Media Player"
             Start-Process -FilePath:$vlcoi -ArgumentList /quiet -Verbose -Wait
-        }else {
+        }
+        else {
             CheckNetworkStatus
             Write-Status -Types "+", $TweakType -Status "Downloading VLC Media Player"
             Start-BitsTransfer -Source $Package2dl -Destination $package2lc -TransferType Download -RetryInterval 60 -RetryTimeout 60 -Verbose | Out-Host
             Check
             Write-Status -Types "+", $TweakType -Status "Installing VLC Media Player"
             Start-Process -FilePath:$package2lc -ArgumentList /quiet -Verbose -Wait
-        }} else {
+        }
+    }
+    else {
         Write-Status -Types "?", $TweakType -Status "VLC Media Player is already Installed on this PC." -Warning
     }
         
@@ -54,14 +60,17 @@ Function Programs() {
         If (Test-Path -Path:$zoomoi) {
             Write-Status -Types "+", $tweaktype -Status "Installing Zoom"
             Start-Process -FilePath:$zoomoi -ArgumentList /quiet -Verbose -Wait
-        } else {
+        }
+        else {
             CheckNetworkStatus
             Write-Status -Types "+", $TweakType -Status "Downloading Zoom"
             Start-BitsTransfer -Source $Package3dl -Destination $package3lc -TransferType Download -RetryInterval 60 -RetryTimeout 60  -Verbose | Out-Host
             Check
             Write-Status -Types "+", $TweakType -Status "Installing Zoom"
             Start-Process -FilePath:$package3lc -ArgumentList /quiet -Verbose -Wait
-        }} else {
+        }
+    }
+    else {
         Write-Status -Types "?", $TweakType -Status "Zoom is already Installed on this PC." -Warning
     }
         
@@ -71,16 +80,19 @@ Function Programs() {
         If (Test-Path -Path:$aroi) {
             Write-Status -Types "+", $TweakType -Status "Installing Adobe Acrobat Reader x64" 
             Start-Process -FilePath:$aroi -ArgumentList /sPB -Verbose
-        }else {
+        }
+        else {
             CheckNetworkStatus
             Write-Status -Types "+", $TweakType -Status "Downloading Adobe Acrobat Reader x64"
             Start-BitsTransfer -Source $Package4dl -Destination $package4lc -TransferType Download -RetryInterval 60 -RetryTimeout 60 -Verbose | Out-Host
             Check
-            If ($? -eq $true){
+            If ($? -eq $true) {
                 Write-Status -Types "+", $TweakType -Status "Installing Adobe Acrobat Reader x64"
                 Start-Process -FilePath:$package4lc -ArgumentList /sPB -Verbose    
             }
-        }} else {
+        }
+    }
+    else {
         Write-Status -Types "?", $TweakType -Status "Adobe Acrobat is already Installed on this PC." -warning
     }
 
